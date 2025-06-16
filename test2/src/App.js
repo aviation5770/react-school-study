8f818e7140e06600375956dcc3049a20c6605035
aviation5770 <aviation5770@dgsw.hs.kr>
2025-06-16 10:30:01 +0900

    feat: state 사용하기

---
+++ test2/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import  Hello from './component/Hello'
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
